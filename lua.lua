22af7e3b930abed0da61322771000ee309bd9eae
NorgumiBackup <erocgreen2006@gmail.com>
2025-02-14 06:11:25 -0600

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,12 +1,3 @@
--- Track & Field: Infinite Script made by mxkxkks
-
--- DO NOT USE WALKSPEED SCRIPTS WHILE USING THIS SCRIPT OR IT WILL GET FLAGGED AS TELEPORTING!
--- Leave the vector values alone if you don't know what you're doing. You can lower the values if you encounter a ban.
--- Please leave everything as it is to avoid kick detection.
--- Script revamped finish line reach since some kids can't comprehend the main point of the script.
--- Made more visual.
--- Resize checkpoints.
-
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
 
